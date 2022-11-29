3f5cbc98bebd6a8de4003b0f74d08918a2da9fa9
Jason Brennan <jason@jasonbrennan.com>
2022-11-28 21:20:04 -0800

    add base app layout for head, header, footer
    add tailwindcss
    
    add homelab page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://jasonbrennan.com',
-	integrations: [mdx({ drafts: true }), sitemap()],
+  site: 'https://jasonbrennan.com',
+  integrations: [mdx({
+    drafts: true
+  }), sitemap(), tailwind()]
 });
---
+++ src/pages/home-lab/index.md
@@ -0,0 +1,9 @@
+---
+layout: "../../layouts/HomeLab.astro"
+title: "I  aaa"
+description: ""
+pubDate: "Nov 25 2022"
+draft: true
+---
+
+test
