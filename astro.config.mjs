ceb3e6fee52de060d0ef61b4f2b3b1d745dd7ee7
Jason Brennan <jason@jasonbrennan.com>
2023-05-07 14:30:23 -0700

    add tryhackme stats and rooms completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-// import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jasonbrennan.com',
-  integrations: [
-    mdx({ drafts: true }),
-    sitemap(),
-    //tailwind()
-  ]
+  integrations: [mdx({
+    drafts: true
+  }), sitemap(), tailwind()]
 });
